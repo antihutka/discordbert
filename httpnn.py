0fb6c1add4ff526c5d2470882c1566628fccc5ca
Michal Povinsky <antihutka@gmail.com>
2019-07-01 13:09:03 +0200

    Retry in backend interface

--- httpnn.py
+++ httpnn.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+from util import aretry
 
 class HTTPNN:
   def __init__(self, url, keyprefix):
@@ -15,18 +16,26 @@ class HTTPNN:
   async def queued_for_key(self, key):
     return len(self.get_lock(key)._waiters)
 
+  @aretry(5)
+  async def put_(self, key, message):
+    async with self.client.post(self.url + "put", json={'key': self.keyprefix + ':' + key, 'text': message}) as response:
+      assert response.status == 200
+      rj = await response.json()
+
   async def put(self, key, message):
     async with self.get_lock(key):
-      async with self.client.post(self.url + "put", json={'key': self.keyprefix + ':' + key, 'text': message}) as response:
-        assert response.status == 200
-        rj = await response.json()
+      return await self.put_(key, message)
+
+  @aretry(5)
+  async def get_(self, key, bad_words):
+    async with self.client.post(self.url + 'get', json={'key': self.keyprefix + ':' + key, 'bad_words': bad_words}) as response:
+      assert response.status == 200
+      rj = await response.json()
+    return rj['text']
 
   async def get(self, key, bad_words = []):
     async with self.get_lock(key):
-      async with self.client.post(self.url + 'get', json={'key': self.keyprefix + ':' + key, 'bad_words': bad_words}) as response:
-        assert response.status == 200
-        rj = await response.json()
-    return rj['text']
+      return await self.get_(key, bad_words)
 
   async def initialize(self):
     self.client = aiohttp.ClientSession(loop = asyncio.get_event_loop(), timeout = aiohttp.ClientTimeout(900))
