0162296cc2920ef551959ac34063b3be309d1f59
Michal Povinsky <antihutka@gmail.com>
2020-10-31 05:23:04 +0100

    Clean up options code

--- options.py
+++ options.py
@@ -1,22 +1,25 @@
 from cachetools import cached, TTLCache
 from cachetools.keys import hashkey
+from collections import namedtuple
 
 from sobutils.database import with_cursor
 
-user_options = ['reply_prob', 'mention_only', 'prefix_only', 'extra_prefix', 'max_bot_msg_length']
+ChatOption = namedtuple('ChatOption', 'name type settable default_user default_group contexts description')
 
-option_types = {
-  'reply_prob': float,
-  'mention_only': int,
-  'prefix_only': int,
-  'extra_prefix': str,
-  'max_bot_msg_length' : int,
-  'reply_to_bots' : int,
-  'ignore_channel': int,
-  'is_bad': int,
-  'is_hidden': int,
-  'blacklisted': int
-}
+options_list = [
+  ChatOption(name='reply_prob',         type=float, settable=True,  default_user=1.0, default_group=0.0, contexts=('channel', 'server'),         description='Probability of replying to any text message'),
+  ChatOption(name='mention_only',       type=int,   settable=True,  default_user=0,   default_group=1,   contexts=('channel', 'server'),         description="Only treat the bot's mention as a trigger instead of name"),
+  ChatOption(name='prefix_only',        type=int,   settable=True,  default_user=0,   default_group=1,   contexts=('channel', 'server'),         description='Only recognize triggers as prefixes instead of anywhere in the message'),
+  ChatOption(name='extra_prefix',       type=str,   settable=True,  default_user='',  default_group='',  contexts=('channel', 'server', 'user'), description='Trigger responses by an additional prefix'),
+  ChatOption(name='max_bot_msg_length', type=int,   settable=True,  default_user=200, default_group=200, contexts=('channel', 'server', 'user'), description="Process (but don't respond to) messages shorter than N characters"),
+  ChatOption(name='reply_to_bots',      type=int,   settable=False, default_user=0,   default_group=0,   contexts=('channel', 'server', 'user'), description=''),
+  ChatOption(name='ignore_channel',     type=int,   settable=False, default_user=0,   default_group=0,   contexts=('channel'),         description=''),
+  ChatOption(name='is_bad',             type=int,   settable=False, default_user=0,   default_group=0,   contexts=('channel'),         description=''),
+  ChatOption(name='is_hidden',          type=int,   settable=False, default_user=0,   default_group=0,   contexts=('channel'),         description=''),
+  ChatOption(name='blacklisted',        type=int,   settable=False, default_user=0,   default_group=0,   contexts=('channel'),         description='')
+]
+
+options = { o.name : o for o in options_list }
 
 default_user = {
   'reply_prob': 1,
@@ -62,13 +65,10 @@ def get_all_options(cursor, channel_id):
 def get_option(server_id, channel_id, option_name):
   opts = get_all_options(channel_id)
   if option_name in opts:
-    #print('Returning set option %s %s -> %s' % (channel_id, option_name, opts[option_name]))
     return opts[option_name]
   if server_id:
-    #print('Returning group default %s %s -> %s' % (channel_id, option_name, default_group[option_name]))
-    return default_group[option_name]
-  #print('Returning user default %s %s -> %s' % (channel_id, option_name, default_user[option_name]))
-  return default_user[option_name]
+    return options[option_name].default_group
+  return options[option_name].default_user
 
 class OptionError(Exception):
   pass
@@ -78,14 +78,14 @@ def set_option_db(cursor, channel_id, option_name, value_parsed):
   cursor.execute('INSERT INTO options2 (channel_id, ' + option_name + ') VALUES (%s,%s) ON DUPLICATE KEY UPDATE ' + option_name + ' = %s', (channel_id, value_parsed, value_parsed))
 
 def set_option(channel_id, option_name, value, user_only = True):
-  if ((option_name not in option_types) or
-      (user_only and option_name not in user_options)
+  if ((option_name not in options) or
+      (user_only and not options[option_name].settable)
      ):
     raise OptionError("Unknown option: %s" % repr(option_name))
   try:
-    value_parsed = None if value is None else option_types[option_name](value)
+    value_parsed = None if value is None else options[option_name].type(value)
   except ValueError:
-    raise OptionError("Can't parse value %s as %s" % (repr(option_name), option_types[option_name].__name__))
+    raise OptionError("Can't parse value %s as %s" % (repr(option_name), options[option_name].type.__name__))
   optioncache.pop(hashkey(channel_id), None)
   try:
     set_option_db(channel_id, option_name, value_parsed)
