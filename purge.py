9b7e80800b7e133a76e5304676cdabeebb8a5cf2
Michal Povinsky <antihutka@gmail.com>
2020-11-11 09:55:42 +0100

    More work on purge

--- purge.py
+++ purge.py
@@ -5,7 +5,15 @@ from sobutils.database import with_cursor, cache_on_commit
 Config.read(sys.argv[1])
 
 def get_batch(cur, lastid, cnt):
-  cur.execute("SELECT id FROM chat WHERE id > %s ORDER BY id ASC LIMIT %s", (lastid, cnt))
+  cur.execute("SELECT id, "
+              "  UNHEX(SHA2(message,256)) IN (SELECT hash FROM bad_messages) AS msg_bad, "
+              "  user_id IN (SELECT id FROM bots) AND user_id NOT IN (SELECT id FROM good_bots) AS msg_bot, "
+              "  count, message "
+              "FROM chat "
+              "LEFT JOIN chat_hashcounts ON UNHEX(SHA2(message,256)) = hash "
+              "WHERE id > %s "
+              "ORDER BY id "
+              "ASC LIMIT %s", (lastid, cnt))
   return cur.fetchall()
 
 @with_cursor
@@ -18,5 +26,9 @@ def scan_db(cur):
       break
     print("Got %d messages ids <%d,%d>" % (len(batch), batch[0][0], batch[-1][0]))
     lastid = batch[-1][0]
+    for messagerow in batch:
+      (mid, mbad, mbot, mcount, mtext) = messagerow
+      if mbad and mbot:
+        print('Message %d is bot and bad! count=%d (%s)' % (mid, mcount, mtext))
 
 scan_db()
