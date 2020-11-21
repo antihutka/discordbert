aef3da18a214448296f99eb7ce31dfaf7accb5cb
Michal Povinsky <antihutka@gmail.com>
2020-11-21 11:12:05 +0100

    Update more stuff for purge

--- purge.py
+++ purge.py
@@ -6,7 +6,7 @@ from sobutils.database import get_dbcon
 
 Config.read(sys.argv[1])
 
-MsgRow = namedtuple('MsgRow', 'id is_bad is_bot age ch_bad ch_black count first_id channel_id server_id expire_days channel_expire text')
+MsgRow = namedtuple('MsgRow', 'id is_bad is_bot age ch_bad ch_black count first_id user_id channel_id server_id expire_days channel_expire text')
 
 def get_maxid(cur):
   cur.execute("SELECT MAX(id) FROM chat")
@@ -17,7 +17,7 @@ def get_batch(cur, lastid, cnt):
               "  UNHEX(SHA2(message,256)) IN (SELECT hash FROM bad_messages) AS msg_bad, "
               "  user_id IN (SELECT id FROM bots) AND user_id NOT IN (SELECT id FROM good_bots) AS msg_bot, "
               "  TIMESTAMPDIFF(DAY, date, CURRENT_TIMESTAMP) AS age, "
-              "  is_bad, blacklisted, count, message_id, channel_id, server_id, expire_days, delete_after, message "
+              "  is_bad, blacklisted, count, message_id, user_id, channel_id, server_id, expire_days, delete_after, message "
               "FROM chat "
               "LEFT JOIN chat_hashcounts ON UNHEX(SHA2(message,256)) = hash "
               "LEFT JOIN options2 USING (channel_id) "
@@ -46,13 +46,17 @@ def should_delete(msg):
     return True
   if msg.is_bad:
     return True
-  if len(msg.text) > 90:
+  if len(msg.text) > 0:
     return True
   return False
 
 def try_delete(cur, msg):
+  #print('Trying to delete: %s' % (msg,))
   if msg.count > 1 and msg.first_id != msg.id:
+    print('Deleting: %s' % (msg,))
     cur.execute("UPDATE chat_hashcounts SET count = count - 1 WHERE hash=UNHEX(SHA2((SELECT message FROM chat WHERE id=%s),256)) AND message_id <> %s", (msg.id, msg.id))
+    assert(cur.rowcount==1)
+    cur.execute("UPDATE chat_counters SET message_count = message_count - 1 WHERE channel_id = %s AND user_id = %s", (msg.channel_id, msg.user_id))
     assert(cur.rowcount==1)
     cur.execute("DELETE FROM chat WHERE id=%s", (msg.id,))
     assert(cur.rowcount==1)
@@ -60,10 +64,15 @@ def try_delete(cur, msg):
       cur.execute("INSERT INTO deleted_counter (channel_id, del_bot_r) VALUES (%s,1) ON DUPLICATE KEY UPDATE del_bot_r = del_bot_r + 1", (msg.channel_id,))
     else:
       cur.execute("INSERT INTO deleted_counter (channel_id, del_user_r) VALUES (%s,1) ON DUPLICATE KEY UPDATE del_user_r = del_user_r + 1", (msg.channel_id,))
+      cur.execute("UPDATE chat_uniqueness SET last_count = last_count -1 WHERE channel_id=%s", (msg.channel_id,))
+      assert(cur.rowcount==1 or cur.rowcount == 0)
     return True
   if msg.count == 1:
+    print('Deleting: %s' % (msg,))
     assert(msg.first_id == msg.id)
     cur.execute("DELETE FROM chat_hashcounts WHERE hash=UNHEX(SHA2((SELECT message FROM chat WHERE id=%s),256)) AND count=1", (msg.id,))
+    assert(cur.rowcount==1)
+    cur.execute("UPDATE chat_counters SET message_count = message_count - 1 WHERE channel_id = %s AND user_id = %s", (msg.channel_id, msg.user_id))
     assert(cur.rowcount==1)
     cur.execute("DELETE FROM chat WHERE id=%s", (msg.id,))
     assert(cur.rowcount==1)
@@ -71,6 +80,8 @@ def try_delete(cur, msg):
       cur.execute("INSERT INTO deleted_counter (channel_id, del_bot_u) VALUES (%s,1) ON DUPLICATE KEY UPDATE del_bot_u = del_bot_u + 1", (msg.channel_id,))
     else:
       cur.execute("INSERT INTO deleted_counter (channel_id, del_user_u) VALUES (%s,1) ON DUPLICATE KEY UPDATE del_user_u = del_user_u + 1", (msg.channel_id,))
+      cur.execute("UPDATE chat_uniqueness SET last_count = last_count -1 WHERE channel_id=%s", (msg.channel_id,))
+      assert(cur.rowcount==1 or cur.rowcount == 0)
     return True
   return False
 
@@ -94,7 +105,6 @@ def scan_db():
       if should_delete(msg):
         cnt_deletable += 1
         if try_delete(cur, msg):
-          print('Deleting: %s' % (msg,))
           cnt_deleted += 1
     print("Checked %d (%.2f%%) deletable %d (%.2f%%) deleted %d (%.2f%%)" % (cnt_checked, cnt_checked / lastid * 100, cnt_deletable, cnt_deletable/cnt_checked*100, cnt_deleted, cnt_deleted/cnt_checked*100))
     dbcon.commit()
